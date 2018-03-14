156da64135b53899e79cf70b2199989a5dc7a3e0
machinelearningxl <antero.maripuu@gmail.com>
2018-03-14 19:12:12 +0000

    Python Programming Essentials homeworks: Functions
    Signed-off-by: machinelearningxl <antero.maripuu@gmail.com>

--- Week 2 homeworks/Homework 1.py
+++ Week 2 homeworks/Homework 1.py
@@ -18,7 +18,7 @@
 #project-to-distance(2, 7, 4)
 
 def project_to_distance(point_x, point_y, distance):
-    dist_to_origin = (point_x ** 2 + point_y ** 2) ** 0.5
+    dist_to_origin=(point_x ** 2 + point_y ** 2) ** 0.5
     scale=distance / dist_to_origin
     print (point_x * scale, point_y * scale)
 
